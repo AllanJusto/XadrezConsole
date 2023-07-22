1c53c1a5836d9f6868ad285a60eda3e8bb7753cf
allan.firme <justoallan129@gmail.com>
2023-07-22 19:49:42 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- XadrezConsole/xadrez/PosicaoXadrez.cs
+++ XadrezConsole/xadrez/PosicaoXadrez.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using tabuleiro;
+
 
 namespace XadrezConsole.xadrez
 {
-    internal class PosicaoXadrez
+    class PosicaoXadrez
     {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+        
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
     }
 }
